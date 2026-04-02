f489538efa53b37f94826899f7d07d4310b38715
ilyaro <40502115+ilyaro@users.noreply.github.com>
2026-04-03 02:28:24 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
+# Install Oh My Zsh if not present
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+fi
+
+# Clone Powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git \
+  "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
+
 # Copy .zshrc to home
 cp .zshrc "$HOME/.zshrc"
-# Set zsh as default shell
-sudo chsh -s $(which zsh) $(whoami)
